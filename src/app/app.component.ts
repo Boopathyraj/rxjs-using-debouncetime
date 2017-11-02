c8abfe32f79d794a14a4e757ae51f92fce1a141a
Boopathyraj Thangadurai <boopathyraj.t@KGFSL.COM>
2017-11-02 10:15:39 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { SearchService } from './search.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [SearchService]
 })
 export class AppComponent {
-  title = 'app';
+  results: Object;
+  searchTerm$ = new Subject<string>();
+
+  constructor(private searchService: SearchService) {
+    //  alert(JSON.stringify(this.searchTerm$));
+    this.searchService.search(this.searchTerm$)
+      .subscribe(results => {
+        
+        this.results = results.results;
+        alert(JSON.stringify(results) );
+      });
+  }
 }
